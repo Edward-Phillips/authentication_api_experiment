e7f2da9d0c8db728e5d7eeafc9299e0002d57653
Edward Phillips <edd0ddi0@gmail.com>
2020-05-13 14:38:35 +0100

    did some stuff

---
+++ config/initializers/cors.rb
@@ -0,0 +1,7 @@
+Rail.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "localhost:3000"
+    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+  
+end
