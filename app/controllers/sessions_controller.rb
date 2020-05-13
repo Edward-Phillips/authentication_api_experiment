1029a024d5b3d573851a78a047fd6b42f35204bb
FayeCarter <fayecarter333@gmail.com>
2020-05-13 15:48:12 +0100

    Registrations returns json for signed in/failed to signed in user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params['user']['email']).try(:authenticate, params['user']['password'])
+    user = User.find_by(email: params['email']).try(:authenticate, params['password'])
     if user
       session[:user_id] = user.id
       render json: {
@@ -8,11 +8,10 @@ class SessionsController < ApplicationController
         logged_in: true,
         user: user
       }
-    else {
+    else 
       render json: {
         status: 401
       }
-    }
     end
   end
 end
